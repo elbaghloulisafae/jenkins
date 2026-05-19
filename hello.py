9f5b4184e43fd3b00b9765bf75ece1c5ad13c05f
zhenya <zhenya@zhenya.myguest.virtualbox.org>
2026-05-19 10:12:14 +0100

    new file

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+
+print("=" * 4)
+print("bienvenue dans mon premier job jenkins !")
+print("=" * 40)
+
+nom = input("quel est ton nom ?")
+print(f"Bonjour {nom}, ton job jenkins q reussi !")
+
+
+a = 10
+b = 5 
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+
